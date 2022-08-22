b313413e02f607f92b2610a5588a2dada5738ea4
MickelsonKlaus <klausmickelson52@email.com>
2022-08-22 13:19:38 +0100

    feat: added update options routes

--- routes/user/admin/survey.js
+++ routes/user/admin/survey.js
@@ -5,32 +5,20 @@ const SurveyRouter = new Router();
 
 //get survey
 SurveyRouter.get("/", (req, res) => {
-  connection.query(
-    "SELECT ??, ??, ?? FROM `survey` ",
-    ["questionID", "question", "adminID"],
-    (error, results, fields) => {
-      if (error) return res.status(500).json({ error: error });
-      results = results.map((data) => {
-        let question = JSON.parse(data.question);
-        return { ...data, question };
-      });
-      return res.status(200).json({ results });
-    }
-  );
+  connection.query("SELECT * FROM `survey`", (error, results, fields) => {
+    if (error) return res.status(500).json({ error: error });
+    return res.status(200).json({ results });
+  });
 });
 
 //get question by id
 SurveyRouter.get("/:id", (req, res) => {
   const { id } = req.params;
   connection.query(
-    "SELECT ??, ??, ?? FROM `survey` where `questionID` = ?",
-    ["questionID", "question", "adminID", id],
+    "SELECT * FROM `survey` where `questionID` = ?",
+    [id],
     (error, results, fields) => {
       if (error) return res.status(500).json({ error: error });
-      results = results.map((data) => {
-        let question = JSON.parse(data.question);
-        return { ...data, question };
-      });
       return res.status(200).json({ results });
     }
   );
@@ -38,16 +26,14 @@ SurveyRouter.get("/:id", (req, res) => {
 
 //post new question
 SurveyRouter.post("/post_question", (req, res) => {
-  const { data, adminID } = req.body;
-
+  const { data } = req.body;
   connection.query(
     "INSERT INTO `survey` SET ?",
     {
-      question: JSON.stringify(data),
-      adminID,
+      ...data,
     },
     (error) => {
-      //console.log(error);
+      // console.log(error);
       if (!error) {
         return res.status(201).json({
           message: "Question uploaded successfully",
@@ -61,19 +47,40 @@ SurveyRouter.post("/post_question", (req, res) => {
 //update question with id
 SurveyRouter.patch("/update_question/:id", (req, res) => {
   const { id } = req.params;
-  const { data } = req.body;
+  const { question } = req.body;
 
   if (!id) {
     return res.status(400).json({ error: "Question ID required" });
   }
   connection.query(
     "UPDATE `survey` SET `question` = ? WHERE `questionID` = ?",
-    [JSON.stringify(data), id],
+    [question, id],
     (error) => {
       if (error) return res.status(500).json({ error: error });
       return res.status(200).json({
         success: true,
         message: "Question updated successfully",
+      });
+    }
+  );
+});
+
+//update options
+SurveyRouter.patch("/update_options/:id", (req, res) => {
+  const { id } = req.params;
+  const { always, never, sometimes, rarely } = req.body;
+
+  if (!id) {
+    return res.status(400).json({ error: "Question ID required" });
+  }
+  connection.query(
+    "UPDATE `survey` SET `always` = ?, `never` = ?, `rarely` = ?, `sometimes` = ? WHERE `questionID` = ?",
+    [always, never, sometimes, rarely, id],
+    (error) => {
+      if (error) return res.status(500).json({ error: error });
+      return res.status(200).json({
+        success: true,
+        message: "options updated successfully",
       });
     }
   );
